980401f55d45cc830e85d017dd5c5f97f1008065
Bernardo Dias <bernardodiasdacruz@gmail.com>
2022-03-02 23:08:50 -0300

    Revert changes

--- src/api/profile/services/profile.js
+++ src/api/profile/services/profile.js
@@ -6,21 +6,4 @@
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::profile.profile', ({ strapi }) => ({
-  async find(params) {
-    // TO DO:
-    // - fix location sorting order by since date
-    // - do not return profiles with empty user
-    const { results, pagination } = await super.find({
-      ...params,
-      populate: {
-        locations: {
-          orderBy: {
-            since: 'DESC'
-          }
-        }
-      }
-    });
-    return { results, pagination };
-  }
-}));
+module.exports = createCoreService('api::profile.profile');
