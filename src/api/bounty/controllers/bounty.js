f23fe0de99bc52dbeaea9820ea50ce8b16041812
Bernardo Dias <bernardodiasdacruz@gmail.com>
2023-02-20 13:08:10 -0300

    Bounties content type

---
+++ src/api/bounty/controllers/bounty.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  bounty controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::bounty.bounty');
---
+++ src/api/bounty/routes/bounty.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * bounty router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::bounty.bounty');
